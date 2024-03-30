9c4dab75a12ad995f65809458c3bfb1f3d052e77
Hipolito Miranda <hipolito.miranda@elaniin.com>
2024-03-30 14:37:16 -0600

    refactor: remove unused files and configurations

---
+++ apps/password-generator/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Button from "@repo/ui/Button"
+
+const HelloWorld = () => {
+  return (
+    <div>HelloWorld pasword-generator
+      <Button />
+    </div>
+  )
+}
+
+export default HelloWorld
---
+++ packages/ui/src/Button.tsx
@@ -0,0 +1,7 @@
+const Button = () => {
+  return (
+    <div>Button placeholder</div>
+  )
+}
+
+export default Button
